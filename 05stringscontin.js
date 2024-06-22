6256169bebff43fb2600d0dd8713be27197bc780
rohan ghosh <rohansciencer405akr@gmail.com>
2024-06-23 02:58:08 +0530

    strings in local machine

---
+++ 05stringscontin.js
@@ -0,0 +1,42 @@
+//continou after codpaces
+const gamename = new String("rohan-rrr")
+
+console.log(gamename[1]);
+console.log(gamename[2]);
+
+console.log(gamename.__proto__);
+
+
+console.log(gamename.length);
+
+console.log(gamename.toUpperCase());
+
+console.log(gamename.charAt(2));
+
+console.log(gamename.indexOf('h'));
+
+const newstring =  gamename.substring(0,5);
+
+console.log(newstring);
+
+
+const anotherstring = gamename.slice(-9,4);
+
+console.log(anotherstring);
+
+const newstrin1 = "  rohan  "
+console.log(newstrin1);
+
+console.log(newstrin1.trim());
+
+const url = "https://rohan.com/rohan%20ghosh"
+
+console.log(url.replace('%20','-'));
+
+
+console.log(url.includes('rohan'));
+
+console.log(gamename.split('-'));
+
+
+
