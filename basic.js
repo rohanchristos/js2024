02c05893759e94595abe42e8b12560cf4e692817
ROHAN GHOSH <150843901+rohanchristos@users.noreply.github.com>
2025-06-03 02:05:06 +0530

    Local
    adding files

---
+++ basic.js
@@ -0,0 +1,40 @@
+// comment starting javscript
+let a=1;
+let gretting="hellow";
+
+
+function greet(name){
+    console.log("hello"+name);
+}
+
+greet("rohan ghosh");
+
+const age=18;
+
+
+if(age>=18){
+    console.log("adult you can continue");
+
+}else{
+    console.log("you are not adult");
+}
+let sum=0;
+
+for(let i=0;i<10;i++){
+    sum+=i; 
+}
+console.log(sum);
+
+console.log("ran perfectly");
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ delta2.js
@@ -0,0 +1,21 @@
+
+let movie="avtar";
+let guess=prompt("what is the movie guess?");
+while(guess!=movie&&guess!="quit"){
+    console.log("tryagin");
+    guess=prompt("what is the movie guess?");
+}
+if(guess=="quit"){
+    console.log("the answer is avtar");
+}else{
+    console.log("hey bro you guess it right !!!!!!!!");
+}
+console.log("do play  agin");
+
+
+const pinaca={
+    range:5000,
+    name:"pinaca missile Air defense sysytem",
+    origin:"INDIA"
+
+};
